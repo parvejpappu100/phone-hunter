f05d090776a7800ac66845532879cfca355a4382
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-02-27 20:41:53 +0600

    load phone details

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
-const loadPhones = (searchText) =>{
+const loadPhones = (searchText,dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayPhones(data.data))
+    .then(data => displayPhones(data.data , dataLimit))
 }
-const displayPhones = phones =>{
+const displayPhones = (phones,dataLimit) =>{
     const phonesContainer = document.getElementById("phones-container");
     phonesContainer.innerHTML = "";
     // * To display 9 items:
-    phones = phones.slice(0,9);
-    // * Added no found message:
+    const showAll = document.getElementById("show-all");
+    if(dataLimit && phones.length > 9){
+        phones = phones.slice(0,9);
+        showAll.classList.remove("d-none");
+    }
+    else{
+        showAll.classList.add("d-none");
+    }
+    // * Added no found message on display :
     const noPhones = document.getElementById("no-found-message");
-    const showAllButton = document.getElementById("show-all-phone");
     if (phones.length === 0){
         noPhones.classList.remove("d-none");
-        showAllButton.classList.add("d-none");
     }
     else{
         noPhones.classList.add("d-none");
-        showAllButton.classList.remove("d-none");
     }
     // * display phones
     phones.forEach(phone =>{
@@ -30,6 +34,7 @@ const displayPhones = phones =>{
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -39,13 +44,23 @@ const displayPhones = phones =>{
     toggleSpinner(false);
 }
 
-
-const searchPhones = () => {
+const searchProccess = (dataLimit) =>{
     const searchText = document.getElementById("search-field").value ;
     // * Start loder:
     toggleSpinner(true)
-    loadPhones(searchText);
+    loadPhones(searchText,dataLimit);
+}
+
+const searchPhones = () => {
+    searchProccess(9);
 }   
+
+// * search input field enter key handler:
+document.getElementById('search-field').addEventListener("keypress",function(event){
+    if(event.key == "Enter"){
+        searchProccess(9);
+    }
+})
 
 // * Set Loder or Spinner:
 const toggleSpinner = isLoding =>{
@@ -57,5 +72,18 @@ const toggleSpinner = isLoding =>{
         loderSection.classList.add("d-none");
     }
 }
+
+// * Not the best way to load show all:
+document.getElementById("show-all-btn").addEventListener("click",function(){
+    searchProccess();
+})
+
+const loadPhoneDetails = (id) =>{
+    const url =` https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
+}
+
 
 loadPhones("phone");
--- js/showAll.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("show-all-phone").addEventListener("click",function(){
-    window.location.href = "showAll.html";
-});
